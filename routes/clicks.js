5b53009595a72980801a4a1762b46afcdc680622
Kyle Goetz <kylegoetz@gmail.com>
2017-06-18 14:42:37 -0500

    block invalid mac addresses, return all results with GET query

--- routes/clicks.js
+++ routes/clicks.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const models = require('../models');
 
 router.get('/', (req, res, next) => {
-	models.Click.find()
+	models.Click.findAll()
 	.then(results => res.status(200).json(results))
 	.catch(err => res.status(500).send(err));
 });
@@ -11,7 +11,10 @@ router.get('/', (req, res, next) => {
 /* POST click listing. */
 router.post('/:dashMacAddress', function(req, res, next) {
 	//add to DB
-	models.Click.upsert(req.params)
+	if(  req.params.dashMacAddress.length < 12 || isNaN(`0x${req.params.dashMacAddress}`)  ) {
+		return res.status(500).json({success: false, message: 'Not a valid MAC address'});
+	}
+	return models.Click.upsert(req.params)
 	.then(() => {
 		res.status(200).json({success:true});
 	})
